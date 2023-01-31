fd5fededeb456d409fc40c0d4b58c2c486c848bc
Bruno Peixoto <brunolnetto@gmail.com>
2023-01-31 19:15:58 -0300

    feat/ jest test

---
+++ web/test_expect/jest.config.js
@@ -0,0 +1,14 @@
+export default {
+    // The bail config option can be used here to have Jest stop running tests after
+    // the first failure.
+    bail: false,
+  
+    // Indicates whether each individual test should be reported during the run.
+    verbose: false,
+  
+    // Indicates whether the coverage information should be collected while executing the test
+    collectCoverage: false,
+  
+    // The directory where Jest should output its coverage files.
+    coverageDirectory: "./coverage/",
+  };
